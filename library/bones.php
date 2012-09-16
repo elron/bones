f98df1bbc8b78f505c6f805d83f088d08269396e
elron <elron100duck@gmail.com>
2012-09-16 16:47:49 +0300

    added __() to another string i found
    Hey Eddie,
    i found another string.
    i also noticed that theres no "Read" string in the language files. could you add that string (and then i can translate it)?
    
    Thanks!

--- library/bones.php
+++ library/bones.php
@@ -362,7 +362,7 @@ function bones_filter_ptags_on_images($content){
 function bones_excerpt_more($more) {
 	global $post;
 	// edit here if you like
-	return '...  <a href="'. get_permalink($post->ID) . '" title="Read '.get_the_title($post->ID).'">Read more &raquo;</a>';
+		return '...  <a href="'. get_permalink($post->ID) . '" title="'. __('Read', 'bonestheme') . get_the_title($post->ID).'">'. __('Read more &raquo;', 'bonestheme') .'</a>';
 }
 
                   	
